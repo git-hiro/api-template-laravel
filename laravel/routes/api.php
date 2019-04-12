38b9ecb19608dc7348b25fe7d048de37a8a637e0
git-purine <gmai1.hiro@gmail.com>
2019-04-12 17:46:58 +0900

    update routes

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -1,16 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 Route::group(['namespace' => 'V1', 'prefix' => 'v1'], function () {
-    Route::resource('users', 'UsersController', ['only' => ['index', 'show', 'store', 'update', 'destroy']]);
+  Route::resource('users', 'UserController', ['only' => ['index', 'show', 'store', 'update', 'destroy']]);
 });
