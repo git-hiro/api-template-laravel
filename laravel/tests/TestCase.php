19bec2df63f2fcf00652cadede49875bcc0a3417
git-purine <gmai1.hiro@gmail.com>
2019-04-18 12:25:16 +0900

    update TestCase

--- laravel/tests/TestCase.php
+++ laravel/tests/TestCase.php
@@ -6,5 +6,13 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
+  use CreatesApplication;
+
+  public function setup(): void
+  {
+    parent::setUp();
+
+    \Log::info('');
+    \Log::info('--- ' . $this->getName() . ' ---');
+  }
 }
